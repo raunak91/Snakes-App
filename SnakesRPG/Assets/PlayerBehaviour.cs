1d4b49209cf811686c0587fd6c5cca7b9b123229
meeya <ynafey@gmail.com>
2015-10-17 15:22:24 +0530

    Basic Movement

--- SnakesRPG/Assets/PlayerBehaviour.cs
+++ SnakesRPG/Assets/PlayerBehaviour.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
+    
+    public void Move(int i) {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }	
+
+    public void MoveTo(int X, int Y) {
+        if (!(X < 5 && X >= -5)) return;
+        if (!(Y < 5 && Y >= -5)) return;
+
+        this.transform.position = new Vector3(X, Y, this.transform.position.z);
+    }
 }
